55928c27cc3dbfcd485ac79f7f461b4e849bdba9
Pablo Alan <pabloxt14@gmail.com>
2023-09-12 16:02:37 -0300

    style: creating global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
+import { Nunito_Sans } from 'next/font/google'
+
+export const nunitoSans = Nunito_Sans({
+  subsets: ['latin']
+})
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${nunitoSans.className}`}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
