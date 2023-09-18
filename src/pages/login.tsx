7f3452de34ce8e0922a1e5b90baf5c873f0b4053
Pablo Alan <pabloxt14@gmail.com>
2023-09-18 15:59:15 -0300

    feat: created login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,27 @@
+import { AuthButtons } from '@/components/AuthButtons';
+import { Heading, Text } from '@/components/Typography';
+import { LoginContainer, LogoSection, WelcomeSection } from '@/styles/pages/login';
+import Head from 'next/head';
+
+export default function Login() {
+  return (
+    <LoginContainer>
+      <Head>
+        <title>Login | BookWise</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
+      </Head>
+      <LogoSection>
+        <img src="/images/logo.svg" alt="BookWise Logo" />
+      </LogoSection>
+
+      <WelcomeSection>
+        <Heading size="lg" color="gray-100">Boas vindas!</Heading>
+        <Text color="gray-200">
+          Faça seu login ou acesse como visitante.
+        </Text>
+
+        <AuthButtons />
+      </WelcomeSection>
+    </LoginContainer>
+  )
+}
