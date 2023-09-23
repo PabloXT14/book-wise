1b30d92782f827c77d05e37a4c0e79bb2b40b868
Pablo Alan <pabloxt14@gmail.com>
2023-09-23 17:54:44 -0300

    feat: created LatestRatings section component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,15 +3,14 @@ import { useSession, signOut } from 'next-auth/react'
 import { NextPageWithLayout } from './_app'
 import { DefaultLayout } from '@/layouts/DefaultLayout'
 import { HomeContainer } from '@/styles/pages/home'
+import { LatestRatings } from '@/components/LatestRatings'
 
 const HomePage: NextPageWithLayout = () => {
   const { data } = useSession()
 
   return (
     <HomeContainer>
-      <main>
-        main
-      </main>
+      <LatestRatings />
       <aside>
         aside
       </aside>
