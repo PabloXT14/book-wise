817065cfca771f86769c48700a54506502677be3
Pablo Alan <pabloxt14@gmail.com>
2023-09-24 16:16:14 -0300

    feat: created BookCard component

---
+++ src/components/BookCard/index.tsx
@@ -0,0 +1,55 @@
+import { Book } from '@prisma/client'
+import { BookDetails, BookImage, BookName, Container } from './styles'
+import { Text } from '../Typography';
+import { RatingStars } from '../RatingStars';
+
+export type BookWithAvgRating = Book & {
+  avgRating: number;
+}
+
+type BookCardProps = {
+  book: BookWithAvgRating;
+  size?: 'md' | 'lg';
+}
+
+export const BookCard = ({ book, size = 'md' }: BookCardProps) => {
+  const IMAGE_SIZES = {
+    md: {
+      width: 64,
+      height: 94,
+    },
+    lg: {
+      width: 108,
+      height: 152,
+    }
+  }
+
+  const currentSize = IMAGE_SIZES[size]
+
+  return (
+    <Container>
+      <BookImage
+        src={book.cover_url}
+        alt={book.name}
+        width={currentSize.width}
+        height={currentSize.height}
+        css={{
+          minWidth: IMAGE_SIZES[size].width
+        }}
+      />
+
+      <BookDetails>
+        <div>
+          <BookName size="xs">
+            {book.name}
+          </BookName>
+          <Text size="sm" color="gray-400">
+            {book.author}
+          </Text>
+        </div>
+
+        <RatingStars rating={book.avgRating} />
+      </BookDetails>
+    </Container>
+  )
+}
