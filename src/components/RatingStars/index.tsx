102b9486c15aca585b4b3f6f7514fac5ed8d7500
Pablo Alan <pabloxt14@gmail.com>
2023-09-29 17:12:30 -0300

    feat: add setRating logic on RatingStars component

--- src/components/RatingStars/index.tsx
+++ src/components/RatingStars/index.tsx
@@ -1,19 +1,47 @@
+import { useState } from 'react'
 import { ComponentProps } from '@stitches/react'
-import { Container } from './styles'
 import { Star } from '@phosphor-icons/react'
+
+
+import { Container } from './styles'
 
 type RatingStarProps = ComponentProps<typeof Container> & {
   rating: number;
   size?: 'sm' | 'md' | 'lg'
-}
+  setRating?: (rating: number) => void
+} 
 
-export const RatingStars = ({ rating, size = 'sm', ...props }: RatingStarProps) => {
+export const RatingStars = ({ rating, size = 'sm', setRating, ...props }: RatingStarProps) => {
+  const [previewValue, setPreviewValue] = useState(0)
+  const isEditable = !!setRating
+
+  const ratingValue = isEditable ? previewValue : rating
+
+  const handleMouseEnter = (value: number) => {
+    if (isEditable) setPreviewValue(value)
+  }
+
+  const handleMouseLeave = () => {
+    if (isEditable) setPreviewValue(rating)
+  }
+
+  const handleSetValue = () => {
+    if (isEditable) setRating(ratingValue)
+  }
+
   return (
-    <Container size={size} {...props}>
-      {Array.from({ length: 5 }).map((_, i) => (
+    <Container
+      css={isEditable ? { cursor: 'pointer' } : undefined}
+      size={size}
+      {...props}
+    >
+      {Array.from({ length: 5 }).map((_, index) => (
         <Star
-          key={`star-${i}`}
-          weight={(i + 1) <= rating ? 'fill' : 'regular'}
+          key={`star-${index}`}
+          weight={(index + 1) <= ratingValue ? 'fill' : 'regular'}
+          onMouseEnter={() => handleMouseEnter(index + 1)}
+          onMouseLeave={handleMouseLeave}
+          onClick={handleSetValue}
         />
       ))}
     </Container>
