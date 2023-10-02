060cb5e1681b11f6220fde229157ae228ba62517
Pablo Alan <pabloxt14@gmail.com>
2023-10-02 15:53:07 -0300

    feat: remaining logics of the ProfileRatings component

--- src/components/ProfileRatings/index.tsx
+++ src/components/ProfileRatings/index.tsx
@@ -1,9 +1,70 @@
-import { Container } from './styles'
+import { useState, useMemo } from 'react'
+import { Book, CategoriesOnBooks, Category, Rating } from '@prisma/client'
+import { useDebounce } from '@uidotdev/usehooks'
 
-export const ProfileRatings = () => {
+import { Container, RatingList } from './styles'
+import { PageTitle } from '../ui/PageTitle'
+import { MagnifyingGlass, User } from '@phosphor-icons/react'
+import { Link } from '../ui/Link'
+import { Input } from '../ui/Form/Input'
+import { ProfileRatingCard } from './ProfileRatingCard'
+import { Text } from '../Typography'
+
+export type ProfileRating = Rating & {
+  book: Book & {
+    categories: CategoriesOnBooks & {
+      category: Category
+    }[]
+  }
+}
+
+type ProfileRatingsProps = {
+  ratings: ProfileRating[]
+  isOwnProfile: boolean
+}
+
+export const ProfileRatings = ({ ratings, isOwnProfile }: ProfileRatingsProps) => {
+  const [search, setSearch] = useState('')
+
+  const debouncedSearch = useDebounce(search, 500)
+
+  const filteredRatings = useMemo(() => {
+    return ratings?.filter((rating) => {
+      return rating.book.name.toLowerCase().includes(debouncedSearch.toLowerCase())
+    })
+  }, [ratings, debouncedSearch])
+
   return (
     <Container>
-      profile
+      {isOwnProfile ? (
+        <PageTitle title="Perfil" icon={<User size={25} />} />
+      ) : (
+        <Link
+          href="/"
+          text="Voltar"
+          iconSide="left"
+          color="white"
+          css={{ alignSelf: 'flex-start' }}
+        />
+      )}
+      <Input
+        placeholder="Buscar livro avaliado"
+        icon={<MagnifyingGlass size={20} />}
+        css={{ marginTop: 40, marginBottom: 32 }}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <RatingList>
+        {filteredRatings?.map((rating) => (
+          <ProfileRatingCard key={rating.id} rating={rating} />
+        ))}
+
+        {filteredRatings.length <= 0 && (
+          <Text color="gray-400" css={{ textAlign: 'center' }}>
+            {debouncedSearch ? 'Nenhum resultado encontrado' : 'Nenhuma avaliação encontrada'}
+          </Text>
+        )}
+      </RatingList>
     </Container>
   )
 }
