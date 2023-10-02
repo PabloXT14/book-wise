9ba3a197065afa674c437f23b1e12cf822c3393d
Pablo Alan <pabloxt14@gmail.com>
2023-10-02 16:32:25 -0300

    feat: remaining base profile page implementations

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -1,18 +1,34 @@
 import { ReactElement } from 'react';
 import { useRouter } from 'next/router';
 import { useQuery } from '@tanstack/react-query';
+import { useSession } from 'next-auth/react'
 
 import { NextPageWithLayout } from '../_app';
 import { DefaultLayout } from '@/layouts/DefaultLayout';
 import { HomeContainer } from '@/styles/pages/home';
 import { api } from '@/lib/axios';
-import { ProfileRatings } from '@/components/ProfileRatings';
+import { ProfileRating, ProfileRatings } from '@/components/ProfileRatings';
+import { ProfileDetails } from '@/components/ProfileDetails';
+
+export type ProfileData = {
+  user: {
+    avatar_url: string
+    name: string
+    member_since: string
+  },
+  ratings: ProfileRating[],
+  readPages: number
+  ratedBooks: number
+  readAuthors: number
+  mostReadCategory?: string
+}
 
 const ProfilePage: NextPageWithLayout = () => {
+  const { data: session } = useSession()
   const router = useRouter()
   const userId = router.query.id as string
 
-  const { data: profile } = useQuery({
+  const { data: profile } = useQuery<ProfileData>({
     queryKey: ['profile', userId],
     queryFn: async () => {
       const response = await api.get(`/profile/${userId}`)
@@ -22,11 +38,18 @@ const ProfilePage: NextPageWithLayout = () => {
     enabled: !!userId
   })
 
-  console.log(profile)
+  const isOwnProfile = session?.user.id === userId
 
   return (
     <HomeContainer>
-      <ProfileRatings />
+      {!!profile ? (
+        <>
+          <ProfileRatings ratings={profile.ratings} isOwnProfile={isOwnProfile} />
+          <ProfileDetails profile={profile} />
+        </>
+      ) : (
+        <h1>Carregando...</h1>
+      )}
     </HomeContainer>
   )
 }
